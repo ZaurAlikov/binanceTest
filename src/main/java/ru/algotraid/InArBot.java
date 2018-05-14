322477f509f4d68e3924331b4b49365e0eef2937
z.alikov <Pu*7sRq2>
2018-05-14 23:31:26 +0300

    Уменьшил количество обращений к серверу

--- src/main/java/ru/algotraid/InArBot.java
+++ src/main/java/ru/algotraid/InArBot.java
@@ -1,10 +1,14 @@
 package ru.algotraid;
 
+import com.binance.api.client.domain.market.TickerPrice;
 import com.binance.api.client.impl.BinanceApiRestClientImpl;
+
+import java.util.List;
+import java.util.Optional;
 
 public class InArBot {
 
-    private Double initCoin = 1000.0;
+    private Double initCoin = 100.0;
     private Double commission;
     private BinanceApiRestClientImpl apiRestClient;
 
@@ -18,23 +22,37 @@ public class InArBot {
         Double firstBuy;
         Double secondBuy;
         Double thirdBuy;
+        Double firstPairPrice;
+        Double secondPairPrice;
+        Double thirdPairPrice;
 
-        Double firstPairPrice = Double.valueOf(apiRestClient.getPrice(firstPair).getPrice());
-        Double secondPairPrice = Double.valueOf(apiRestClient.getPrice(secondPair).getPrice());
-        Double thirdPairPrice = Double.valueOf(apiRestClient.getPrice(thirdPair).getPrice());
-        if (directCalc) firstBuy = withCommission(initCoin) / firstPairPrice;
-        else firstBuy = withCommission(initCoin) * firstPairPrice;
-        if (secondPair.contains("BTC") || secondPair.contains("ETH")){
-            secondBuy = withCommission(firstBuy) / secondPairPrice;
-        } else {
-            secondBuy = withCommission(firstBuy) * secondPairPrice;
+        List<TickerPrice> prices = apiRestClient.getAllPrices();
+
+        firstPairPrice = getPrice(prices, firstPair);
+        secondPairPrice = getPrice(prices, secondPair);
+        thirdPairPrice = getPrice(prices, thirdPair);
+
+        if (firstPairPrice != 0.0 && secondPairPrice != 0.0 && thirdPairPrice !=0.0){
+            if (directCalc) firstBuy = withCommission(initCoin) / firstPairPrice;
+            else firstBuy = withCommission(initCoin) * firstPairPrice;
+            if (secondPair.contains("BTC") || secondPair.contains("ETH")){
+                secondBuy = withCommission(firstBuy) / secondPairPrice;
+            } else {
+                secondBuy = withCommission(firstBuy) * secondPairPrice;
+            }
+            if (directCalc) thirdBuy = withCommission(secondBuy) * thirdPairPrice;
+            else thirdBuy = withCommission(secondBuy) / thirdPairPrice;
+            return thirdBuy - initCoin;
         }
-        if (directCalc) thirdBuy = withCommission(secondBuy) * thirdPairPrice;
-        else thirdBuy = withCommission(secondBuy) / thirdPairPrice;
-        return thirdBuy - initCoin;
+        return 0.0;
     }
 
     private Double withCommission(Double withoutCommission){
         return withoutCommission - (withoutCommission * commission);
+    }
+
+    private Double getPrice(List<TickerPrice> prices, String pair){
+        Optional<TickerPrice> tickerPrice = prices.stream().filter(s -> s.getSymbol().equals(pair)).findFirst();
+        return tickerPrice.map(tickerPrice1 -> Double.valueOf(tickerPrice1.getPrice())).orElse(0.0);
     }
 }
