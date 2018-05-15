1bada08e53f94f6855960ffbaad3b181abfd1c0f
z.alikov <Pu*7sRq2>
2018-05-15 21:08:40 +0300

    Небольшие правки

--- src/main/java/ru/algotraid/InArBot.java
+++ src/main/java/ru/algotraid/InArBot.java
@@ -1,12 +1,14 @@
 package ru.algotraid;
 
 import com.binance.api.client.domain.account.NewOrder;
+import com.binance.api.client.domain.account.NewOrderResponse;
 import com.binance.api.client.domain.general.ExchangeInfo;
 import com.binance.api.client.domain.general.SymbolInfo;
 import com.binance.api.client.domain.market.TickerPrice;
 import com.binance.api.client.impl.BinanceApiRestClientImpl;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 public class InArBot {
@@ -14,6 +16,9 @@ public class InArBot {
     private Double commission;
     private BinanceApiRestClientImpl apiRestClient;
     private ExchangeInfo exchangeInfo;
+    private Double firstPairPrice;
+    private Double secondPairPrice;
+    private Double thirdPairPrice;
 
     public InArBot(String apiKey, String secretKey, Boolean BNBCommission) {
         this.apiRestClient = new BinanceApiRestClientImpl(apiKey, secretKey);
@@ -26,9 +31,7 @@ public class InArBot {
         Double firstBuy;
         Double secondBuy;
         Double thirdBuy;
-        Double firstPairPrice;
-        Double secondPairPrice;
-        Double thirdPairPrice;
+
 
         List<TickerPrice> prices = apiRestClient.getAllPrices();
 
@@ -55,13 +58,44 @@ public class InArBot {
         return Double.valueOf(apiRestClient.getAccount(100000L, System.currentTimeMillis()).getAssetBalance(symbol).getFree());
     }
 
-    public void buyCicle(String firstPair, String secondPair, String thirdPair, Double sumForTrade){
+    public void buyCycle(Double sumForTrade, String firstPair, String secondPair, String thirdPair, boolean directBuy){
+        Double countPayCoins;
+        countPayCoins = buyCoins(sumForTrade, firstPair, firstPairPrice, directBuy, 1);
+        countPayCoins = buyCoins(countPayCoins, secondPair, secondPairPrice, directBuy ,2);
+        countPayCoins = buyCoins(countPayCoins, thirdPair, thirdPairPrice, directBuy ,3);
+        System.out.println("Profit = " + countPayCoins);
+    }
 
-        SymbolInfo firstPairInfo = exchangeInfo.getSymbolInfo(firstPair);
-        SymbolInfo secondPairInfo = exchangeInfo.getSymbolInfo(secondPair);
-        SymbolInfo thirdPairInfo = exchangeInfo.getSymbolInfo(thirdPair);
+    private Double buyCoins(Double sumForTrade, String pair, Double pairPrice, boolean direct, int numPair){
+        Double pairQuantity = 0.0;
+        switch (numPair){
+            case 1:
+                if (direct) pairQuantity = withCommission(sumForTrade) / pairPrice;
+                else pairQuantity = withCommission(sumForTrade) * pairPrice;
+            case 2:
+                if (pair.contains("BTC") || pair.contains("ETH")) {
+                    pairQuantity = withCommission(sumForTrade) / pairPrice;
+                } else {
+                    pairQuantity = withCommission(sumForTrade) * pairPrice;
+                }
+            case 3:
+                if (direct) pairQuantity = withCommission(sumForTrade) * pairPrice;
+                else pairQuantity = withCommission(sumForTrade) / pairPrice;
+        }
 
-        apiRestClient.newOrderTest(NewOrder.marketBuy(firstPair, sumForTrade.toString()));
+//        if (pairQuantity == 0.0){
+            SymbolInfo pairInfo = exchangeInfo.getSymbolInfo(pair);
+            Double step = Double.valueOf(pairInfo.getFilters().get(1).getStepSize());
+            String normalQuantity = String.format(Locale.UK,"%.8f", Math.floor(pairQuantity / step) * step);
+//        NewOrderResponse response = apiRestClient.newOrder(NewOrder.marketBuy(pair, normalQuantity));
+//        return Double.valueOf(response.getExecutedQty());
+//        } else return 0.0;
+
+
+        // for tests
+        apiRestClient.newOrderTest(NewOrder.marketBuy(pair, normalQuantity));
+        return Double.valueOf(normalQuantity);
+
     }
 
     private Double withCommission(Double withoutCommission) {
--- src/main/java/ru/algotraid/Main.java
+++ src/main/java/ru/algotraid/Main.java
@@ -19,102 +19,116 @@ public class Main {
             System.out.println(balanceCache.getAccountBalanceCache().get("USDT").getFree());
 //            System.out.println(inArBot.getFreeBalance("USDT"));
 
-            profit = inArBot.getProfit(bet,"ADAUSDT", "ADABNB", "BNBUSDT", true);
+            profit = inArBot.getProfit(bet, "ADAUSDT", "ADABNB", "BNBUSDT", true);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("ADAUSDT", "ADAUSDT", "BNBUSDT", bet);
                 System.out.println("USDT -> ADA -> BNB -> USDT " + profit);
+                inArBot.buyCycle(bet,"ADAUSDT", "ADAUSDT", "BNBUSDT", true);
+
             }
 
-            profit = inArBot.getProfit(bet,"BCCUSDT", "BCCBNB", "BNBUSDT", true);
+            profit = inArBot.getProfit(bet, "BCCUSDT", "BCCBNB", "BNBUSDT", true);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("BCCUSDT", "BCCBNB", "BNBUSDT", bet);
                 System.out.println("USDT -> BCC -> BNB -> USDT " + profit);
+                inArBot.buyCycle(bet,"BCCUSDT", "BCCBNB", "BNBUSDT", true);
+
             }
 
-            profit = inArBot.getProfit(bet,"BTCUSDT", "BNBBTC", "BNBUSDT", true);
+            profit = inArBot.getProfit(bet, "BTCUSDT", "BNBBTC", "BNBUSDT", true);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("BTCUSDT", "BNBBTC", "BNBUSDT", bet);
                 System.out.println("USDT -> BTC -> BNB -> USDT " + profit);
+                inArBot.buyCycle(bet,"BTCUSDT", "BNBBTC", "BNBUSDT", true);
+
             }
 
-            profit = inArBot.getProfit(bet,"ETHUSDT", "BNBETH", "BNBUSDT", true);
+            profit = inArBot.getProfit(bet, "ETHUSDT", "BNBETH", "BNBUSDT", true);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("ETHUSDT", "BNBETH", "BNBUSDT", bet);
                 System.out.println("USDT -> ETH -> BNB -> USDT " + profit);
+                inArBot.buyCycle(bet,"ETHUSDT", "BNBETH", "BNBUSDT", true);
+
             }
 
-            profit = inArBot.getProfit(bet,"LTCUSDT", "LTCBNB", "BNBUSDT", true);
+            profit = inArBot.getProfit(bet, "LTCUSDT", "LTCBNB", "BNBUSDT", true);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("LTCUSDT", "LTCBNB", "BNBUSDT", bet);
                 System.out.println("USDT -> LTC -> BNB -> USDT " + profit);
+                inArBot.buyCycle(bet,"LTCUSDT", "LTCBNB", "BNBUSDT", true);
+
             }
 
-            profit = inArBot.getProfit(bet,"NEOUSDT", "NEOBNB", "BNBUSDT", true);
+            profit = inArBot.getProfit(bet, "NEOUSDT", "NEOBNB", "BNBUSDT", true);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("NEOUSDT", "NEOBNB", "BNBUSDT", bet);
                 System.out.println("USDT -> NEO -> BNB -> USDT " + profit);
+                inArBot.buyCycle(bet,"NEOUSDT", "NEOBNB", "BNBUSDT", true);
+
             }
 
-            profit = inArBot.getProfit(bet,"QTUMUSDT", "QTUMBNB", "BNBUSDT", true);
+            profit = inArBot.getProfit(bet, "QTUMUSDT", "QTUMBNB", "BNBUSDT", true);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("QTUMUSDT", "QTUMBNB", "BNBUSDT", bet);
                 System.out.println("USDT -> QTUM -> BNB -> USDT " + profit);
+                inArBot.buyCycle(bet,"QTUMUSDT", "QTUMBNB", "BNBUSDT", true);
+
             }
 
-            profit = inArBot.getProfit(bet,"BNBUSDT", "ADABNB", "ADAUSDT", false);
+            profit = inArBot.getProfit(bet, "BNBUSDT", "ADABNB", "ADAUSDT", false);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("BNBUSDT", "ADABNB", "ADAUSDT", bet);
                 System.out.println("USDT -> BNB -> ADA -> USDT " + profit);
+                inArBot.buyCycle(bet,"BNBUSDT", "ADABNB", "ADAUSDT", false);
+
             }
 
-            profit = inArBot.getProfit(bet,"BNBUSDT", "BCCBNB", "BCCUSDT", false);
+            profit = inArBot.getProfit(bet, "BNBUSDT", "BCCBNB", "BCCUSDT", false);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("BNBUSDT", "BCCBNB", "BCCUSDT", bet);
                 System.out.println("USDT -> BNB -> BCC -> USDT " + profit);
+                inArBot.buyCycle(bet,"BNBUSDT", "BCCBNB", "BCCUSDT", false);
+
             }
 
-            profit = inArBot.getProfit(bet,"BNBUSDT", "BNBBTC", "BTCUSDT", false);
+            profit = inArBot.getProfit(bet, "BNBUSDT", "BNBBTC", "BTCUSDT", false);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("BNBUSDT", "BNBBTC", "BTCUSDT", bet);
                 System.out.println("USDT -> BNB -> BTC -> USDT " + profit);
+                inArBot.buyCycle(bet,"BNBUSDT", "BNBBTC", "BTCUSDT", false);
+
             }
 
-            profit = inArBot.getProfit(bet,"BNBUSDT", "BNBETH", "ETHUSDT", false);
+            profit = inArBot.getProfit(bet, "BNBUSDT", "BNBETH", "ETHUSDT", false);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("BNBUSDT", "BNBETH", "ETHUSDT", bet);
                 System.out.println("USDT -> BNB -> ETH -> USDT " + profit);
+                inArBot.buyCycle(bet,"BNBUSDT", "BNBETH", "ETHUSDT", false);
+
             }
 
-            profit = inArBot.getProfit(bet,"BNBUSDT", "LTCBNB", "LTCUSDT", false);
+            profit = inArBot.getProfit(bet, "BNBUSDT", "LTCBNB", "LTCUSDT", false);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("BNBUSDT", "LTCBNB", "LTCUSDT", bet);
                 System.out.println("USDT -> BNB -> LTC -> USDT " + profit);
+                inArBot.buyCycle(bet,"BNBUSDT", "LTCBNB", "LTCUSDT", false);
+
             }
 
-            profit = inArBot.getProfit(bet,"BNBUSDT", "NEOBNB", "NEOUSDT", false);
+            profit = inArBot.getProfit(bet, "BNBUSDT", "NEOBNB", "NEOUSDT", false);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("BNBUSDT", "NEOBNB", "NEOUSDT", bet);
                 System.out.println("USDT -> BNB -> NEO -> USDT " + profit);
+                inArBot.buyCycle(bet,"BNBUSDT", "NEOBNB", "NEOUSDT", false);
+
             }
 
-            profit = inArBot.getProfit(bet,"BNBUSDT", "QTUMBNB", "QTUMUSDT", false);
+            profit = inArBot.getProfit(bet, "BNBUSDT", "QTUMBNB", "QTUMUSDT", false);
             Thread.sleep(100);
             if (profit > step) {
-                inArBot.buyCicle("BNBUSDT", "QTUMBNB", "QTUMUSDT", bet);
                 System.out.println("USDT -> BNB -> QTUM -> USDT " + profit);
+                inArBot.buyCycle(bet,"BNBUSDT", "QTUMBNB", "QTUMUSDT", false);
+
             }
         }
     }
